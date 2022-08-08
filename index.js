73e76ca3abda784259d4e9f7b0880afdb2389fd5
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-08-08 22:04:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+// Write your solution in this file//
+    const employee = {
+        name: "Leslie",
+        streetAddress: "Nairobi",
+    
+};
+function updateEmployeeWithKeyAndValue(employee,key, value) {
+    let newEmployee = {}
+    newEmployee = { ...employee} ;
+    newEmployee[key] = value;
+    return newEmployee;
+
+
+}
+function deleteFromEmployeeByKey(employee, key){
+    let newEmployee = {};
+    newEmployee = { ...employee} ;
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
